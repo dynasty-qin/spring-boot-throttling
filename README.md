b86302a345046c7a99d8ba62acc3ed9f96315b8a
Harry <harry_myemail@163.com>
2020-08-20 18:50:11 +0800

    要星星

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spring-boot-throttling
+
+使用AOP配合Redis对单个IP对某个接口访问频率进行限制, 超出频率后对该IP进行限制访问
+
+觉着有用的话, 请给颗星星 ^_^
--- src/main/java/com/example/throttling/util/IpUtils.java
+++ src/main/java/com/example/throttling/util/IpUtils.java
@@ -6,7 +6,10 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * Author : Harry
- * Description :
+ * Description : 获取用户真实IP
+ * 需配合以下 nginx 配置使用
+ * proxy_set_header        X-Real-IP       $remote_addr;
+ * proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
  * Date : 2020-08-20 16:14
  */
 public class IpUtils {
